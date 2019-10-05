64b229d87198003cde595a60b9a342f4cc155ac4
xiangrui2019 <xiangrui@aiursoft.com>
2019-10-05 05:38:51 -0700

    playground

--- handlers/graph_ql_handler.go
+++ handlers/graph_ql_handler.go
@@ -2,16 +2,15 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 )
 
-func GraphQLHandler(schema *graphql.Schema) gin.HandlerFunc {
-	h := handler.New(&handler.Config{
-		Schema:   schema,
-		Pretty:   true,
-		GraphiQL: true,
-	})
+func GraphQLHandler(config *handler.Config) gin.HandlerFunc {
+	if gin.Mode() != gin.ReleaseMode {
+		config.Playground = true
+	}
+
+	h := handler.New(config)
 
 	return func(context *gin.Context) {
 		h.ServeHTTP(context.Writer, context.Request)
